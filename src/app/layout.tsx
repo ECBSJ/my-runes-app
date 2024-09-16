1098a18fb2c31332d0e8c19e11400a239a8d328b
ecbsj <crypto67sauce@gmail.com>
2024-09-16 11:59:47 -0400

    changed casing to Footer and Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import Footer from "@/components/footer"
+import Footer from "@/components/Footer"
 import ThemeProvider from "@/components/theme-provider"
-import Header from "@/components/header"
+import Header from "@/components/Header"
 
 const inter = Inter({ subsets: ["latin"] })
 
